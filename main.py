0f472c31721ea61386a5fdb50209ba5efc126277
siennawheeler <sienna_wheeler1@my.vcccd.edu>
2024-06-28 02:28:50 +0000

    passed

--- main.py
+++ main.py
@@ -3,21 +3,28 @@ def main():
     while True:
         start = input('Enter the starting letter: ')
         end = input('Enter the ending letter: ')
+        begin = ord(start)
+        stop = ord(end)
+        while stop <= begin or start.isalpha() == False or end.isalpha() == False:
+            start = input()
+            end = input('The ending letter must be after the starting letter: ')
+            begin = ord(start)
+            stop = ord(end)
+        break
+        
 
     """
     ########################################
     Code Your Program here
     ########################################
     """
-    if string.isalpha(start) > string.isalpha(end):
-        start = input('The starting letter must be before the ending letter: ')
-        end = input('The ending letter must be after the starting letter: ')
 
-    begin = string.isalpha(start)
-    stop = string.isalpha(end)
-
-    for x in range(begin, stop):
-        result.append(len(string.isalpha(x)))
+    '''for x in range(begin, stop):
+        result.append(x)'''
+    x = begin
+    while x <= stop:
+        result.append(chr(x))
+        x = x + 1
     print(*result)
 
     ########################################
